958b705376b6d489f32abedf65a3c274aebf9a49
robert-giaquinto <giaquinto.ra@gmail.com>
2019-12-25 10:25:30 -0700

    Updating boosting regularization experiment

--- scripts/boosted_regularization.sh
+++ scripts/boosted_regularization.sh
@@ -1,6 +1,3 @@
-# Hypothesis: More components should lower the loss
-# Experiment: Run planar flows for varying numbers of components
-
 # Load defaults for all experiments
 source ./scripts/experiment_config.sh
 
@@ -9,127 +6,59 @@ source ./venv/bin/activate
 
 # define variable specific to this experiment
 exp_name=regularization
-num_components=6
-flow_length=2
+num_components=2
+epochs=600
+lr=0.0005
 
+for reg in 0.1 0.25 0.5 0.75 0.9 1.0 1.25
+do
+    python main_experiment.py --dataset mnist \
+           --experiment_name ${experiment_name} \
+           --validation \
+           --no_cuda \
+           --num_workers ${num_workers} \
+           --no_lr_schedule \
+           --learning_rate 0.0005 \
+           --epochs ${epochs} \
+           --early_stopping_epochs 0 \
+           --burnin 0 \
+           --annealing_schedule 100 \
+           --flow boosted \
+           --component_type realnvp \
+           --num_base_layers 1 \
+           --base_network relu \
+           --h_size 128 \
+           --num_components ${num_components} \
+           --regularization_rate ${reg} \
+           --num_flows 1 \
+           --z_size ${z_size} \
+           --batch_size ${batch_size} \
+           --manual_seed ${manual_seed} \
+           --plot_interval ${plotting} &
 
-# regularization = 0.01
-python main_experiment.py --dataset ${dataset} \
-       --validation \
-       --experiment_name ${exp_name}1 \
-       --num_workers ${num_workers} \
-       --epochs ${epochs} \
-       --early_stopping_epochs ${early_stop} \
-       --annealing_schedule ${annealing_schedule} \
-       --burnin ${burnin} \
-       --z_size ${z_size} \
-       --flow boosted \
-       --regularization_rate 0.01 \
-       --num_flows ${flow_length} \
-       --component_type ${component_type} \
-       --num_components ${num_components} \
-       --batch_size ${batch_size} \
-       --manual_seed ${seed} \
-       --log_interval ${log} \
-       --plot_interval ${plot} &
+    python main_experiment.py --dataset mnist \
+           --experiment_name ${experiment_name} \
+           --validation \
+           --no_cuda \
+           --num_workers ${num_workers} \
+           --no_lr_schedule \
+           --learning_rate 0.001 \
+           --epochs ${epochs} \
+           --early_stopping_epochs 0 \
+           --burnin 0 \
+           --annealing_schedule 100 \
+           --flow boosted \
+           --component_type realnvp \
+           --num_base_layers 1 \
+           --base_network relu \
+           --h_size 128 \
+           --num_components ${num_components} \
+           --regularization_rate ${reg} \
+           --num_flows 1 \
+           --z_size ${z_size} \
+           --batch_size ${batch_size} \
+           --manual_seed ${manual_seed} \
+           --plot_interval ${plotting} &
 
-# regularization = 0.1
-python main_experiment.py --dataset ${dataset} \
-       --validation \
-       --experiment_name ${exp_name}10 \
-       --num_workers ${num_workers} \
-       --epochs ${epochs} \
-       --early_stopping_epochs ${early_stop} \
-       --annealing_schedule ${annealing_schedule} \
-       --burnin ${burnin} \
-       --z_size ${z_size} \
-       --flow boosted \
-       --regularization_rate 0.1 \
-       --num_flows ${flow_length} \
-       --component_type ${component_type} \
-       --num_components ${num_components} \
-       --batch_size ${batch_size} \
-       --manual_seed ${seed} \
-       --log_interval ${log} \
-       --plot_interval ${plot} &
-
-# regularization = 0.25
-python main_experiment.py --dataset ${dataset} \
-       --validation \
-       --experiment_name ${exp_name}25 \
-       --num_workers ${num_workers} \
-       --epochs ${epochs} \
-       --early_stopping_epochs ${early_stop} \
-       --annealing_schedule ${annealing_schedule} \
-       --burnin ${burnin} \
-       --z_size ${z_size} \
-       --flow boosted \
-       --regularization_rate 0.25 \
-       --num_flows ${flow_length} \
-       --component_type ${component_type} \
-       --num_components ${num_components} \
-       --batch_size ${batch_size} \
-       --manual_seed ${seed} \
-       --log_interval ${log} \
-       --plot_interval ${plot} &
-
-# regularization = 0.5
-python main_experiment.py --dataset ${dataset} \
-       --validation \
-       --experiment_name ${exp_name}50 \
-       --num_workers ${num_workers} \
-       --epochs ${epochs} \
-       --early_stopping_epochs ${early_stop} \
-       --annealing_schedule ${annealing_schedule} \
-       --burnin ${burnin} \
-       --z_size ${z_size} \
-       --flow boosted \
-       --regularization_rate 0.5 \
-       --num_flows ${flow_length} \
-       --component_type ${component_type} \
-       --num_components ${num_components} \
-       --batch_size ${batch_size} \
-       --manual_seed ${seed} \
-       --log_interval ${log} \
-       --plot_interval ${plot} &
-
-# regularization = 1.0
-python main_experiment.py --dataset ${dataset} \
-       --validation \
-       --experiment_name ${exp_name}100 \
-       --num_workers ${num_workers} \
-       --epochs ${epochs} \
-       --early_stopping_epochs ${early_stop} \
-       --annealing_schedule ${annealing_schedule} \
-       --burnin ${burnin} \
-       --z_size ${z_size} \
-       --flow boosted \
-       --regularization_rate 1.0 \
-       --num_flows ${flow_length} \
-       --component_type ${component_type} \
-       --num_components ${num_components} \
-       --batch_size ${batch_size} \
-       --manual_seed ${seed} \
-       --log_interval ${log} \
-       --plot_interval ${plot} &
-
-# regularization = 2.0
-python main_experiment.py --dataset ${dataset} \
-       --validation \
-       --experiment_name ${exp_name}200 \
-       --num_workers ${num_workers} \
-       --epochs ${epochs} \
-       --early_stopping_epochs ${early_stop} \
-       --annealing_schedule ${annealing_schedule} \
-       --burnin ${burnin} \
-       --z_size ${z_size} \
-       --flow boosted \
-       --regularization_rate 2.0 \
-       --num_flows ${flow_length} \
-       --component_type ${component_type} \
-       --num_components ${num_components} \
-       --batch_size ${batch_size} \
-       --manual_seed ${seed} \
-       --log_interval ${log} \
-       --plot_interval ${plot} ;
+done
 
