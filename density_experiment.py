Cleaned up logging to always save a copy of the log. Added option for computing the full fixed G for boosted models using recursive formula, seems better than approximating it and doesn't take too much longer

density_experiment.py
@@ -9,12 +9,13 @@ import logging
 import time
 from tensorboardX import SummaryWriter
 from shutil import copyfile
+from collections import Counter
 
 from optimization.optimizers import init_optimizer
 from models.boosted_flow import BoostedFlow
 from models.realnvp import RealNVPFlow
 from models.glow import Glow
-from utils.utilities import save, load, init_log
+from utils.utilities import save, load, init_log, softmax
 from utils.load_data import load_density_dataset
 from utils.distributions import log_normal_diag, log_normal_standard, log_normal_normalized
 
@@ -51,8 +52,8 @@ parser.add_argument('--experiment_name', type=str, default="density",
 parser.add_argument('--out_dir', type=str, default='./results/snapshots', help='Output directory for model snapshots etc.')
 parser.add_argument('--data_dir', type=str, default='./data/raw/', help="Where raw data is saved.")
 parser.add_argument('--exp_log', type=str, default='./results/density_experiment_log.txt', help='File to save high-level results from each run of an experiment.')
-parser.add_argument('--print_log', dest="save_log", action="store_false", help='Add this flag to have progress printed to log (rather than saved to a file).')
-parser.set_defaults(save_log=True)
+parser.add_argument('--print_log', dest="print_log", action="store_true", help='Add this flag to have progress printed to log (rather than only saved to a file).')
+parser.set_defaults(print_log=True)
 parser.add_argument('--no_tensorboard', dest="tensorboard", action="store_false", help='Turns off saving results to tensorboard.')
 parser.set_defaults(tensorboard=True)
 
@@ -214,7 +215,7 @@ def parse_args(main_args=None):
     args.snap_dir += f'_seed{args.manual_seed}' + lr_schedule + '_' + args.dataset + f"_bs{args.batch_size}"
 
     if args.flow == 'boosted':
-        args.eval_batch_size = 16
+        args.eval_batch_size = 1024 if args.fixed_samples > 1 and args.dataset != "bsds300" else 16
         args.snap_dir += f'_{args.component_type}_C{args.num_components}'
         if args.fixed_samples < 1:
             raise ValueError(f"Number of fixed_samples must be greater than 0, you gave fixed_samples={args.fixed_samples}")
@@ -518,9 +519,25 @@ def evaluate(model, data_loader, args, results_type=None):
     if args.boosted:
         G_nll, g_nll = [], []
         for (x, _) in data_loader:
-            z_G, mu_G, var_G, ldj_G, _ = model(x=x, components="1:c")
-            G_nll_i = -1.0 * (log_normal_standard(z_G, reduce=True, dim=-1, device=args.device) + ldj_G)
-            G_nll.append(G_nll_i.detach())
+
+            if args.fixed_samples == 1:
+                z_G, mu_G, var_G, ldj_G, _ = model(x=x, components="1:c")
+                G_nll_i = -1.0 * (log_normal_standard(z_G, reduce=True, dim=-1, device=args.device) + ldj_G)
+                G_nll.append(G_nll_i.detach())
+
+            else:
+                G_ll = torch.zeros(x.size(0))
+                for c in range(model.component + 1):
+                    z_G, _, _, ldj_G, _ = model(x=x, components=c)
+                    if c == 0:
+                        G_ll = log_normal_standard(z_G, reduce=True, dim=-1, device=args.device) + ldj_G
+                    else:
+                        last_ll = torch.log(1 - model.rho[c]) + G_ll
+                        next_ll = torch.log(model.rho[c]) + (log_normal_standard(z_G, reduce=True, dim=-1, device=args.device) + ldj_G)
+                        uG_ll = torch.cat([last_ll.view(x.size(0), 1), next_ll.view(x.size(0), 1)], dim=1)
+                        G_ll = torch.logsumexp(uG_ll, dim=1)
+                        
+                G_nll.append(-1.0 * G_ll.detach())
 
             # track new component progress just for insights
             if model.component > 0 or model.all_trained:
@@ -557,28 +574,70 @@ def evaluate(model, data_loader, args, results_type=None):
 
 def compute_kl_pq_loss(model, x, args):
     if args.flow == "boosted":
-        z_g, _, _, ldj_g, _ = model(x=x, components="c")
-        g_nll = -1.0 * (log_normal_standard(z_g, reduce=True, dim=-1, device=args.device) + ldj_g)
         
         if model.all_trained or model.component > 0:
-            fixed = '-c' if model.all_trained else '1:c-1'
-            G_nll = []
-            num_samples = model.component if args.fixed_samples == args.num_components else args.fixed_samples
-            for i in range(num_samples):
-                z_G, _, _, ldj_G, _ = model(x=x, components=fixed)
-                G_nll_i = -1.0 * (log_normal_standard(z_G, reduce=True, dim=-1, device=args.device) + ldj_G)
-                G_nll.append(G_nll_i)
-                
-            if args.fixed_samples > 1:
-                G_nll = torch.cat(G_nll)
-                g_nll = torch.cat(num_samples * [g_nll])
-            else:
-                G_nll = G_nll[0]
 
-            losses = {"nll": torch.mean(g_nll - G_nll)}
+            # 1. Compute likelihood/weight for each sample
+            if args.fixed_samples == 1:
+                # randomly sample a component
+                fixed = '-c' if model.all_trained else '1:c-1'
+                z_G, _, _, ldj_G, _ = model(x=x, components=fixed)
+                G_nll = -1.0 * (log_normal_standard(z_G, reduce=True, dim=-1, device=args.device) + ldj_G)
+            else:
+                # combine weighted likelihoods from each component
+                G_ll = torch.zeros(x.size(0))
+                for c in range(model.component):  # TODO: if model.all_trained then what?
+                    z_G, _, _, ldj_G, _ = model(x=x, components=c)
+                    if c == 0:
+                        G_ll = log_normal_standard(z_G, reduce=True, dim=-1, device=args.device) + ldj_G
+                    else:
+                        last_ll = torch.log(1 - model.rho[c]) + G_ll
+                        next_ll = torch.log(model.rho[c]) + (log_normal_standard(z_G, reduce=True, dim=-1, device=args.device) + ldj_G)
+                        uG_ll = torch.cat([last_ll.view(x.size(0), 1), next_ll.view(x.size(0), 1)], dim=1)
+                        G_ll = torch.logsumexp(uG_ll, dim=1)
+                
+                G_nll = -1.0 * G_ll
+
+            reweight_samples = True
+            if reweight_samples:
+                # 2. Sample x with replacement, weighted by G_nll
+                #weights = torch.exp(G_nll - torch.logsumexp(G_nll, dim=0)).numpy()  # normalize weights: large NLL => large weight
+                weights = softmax(G_nll).numpy()            
+                if weights.min() < 0.0:
+                    weights = weights - weights.min()
+                if weights.max() > 0.25:
+                    #weights = weights + 1.0
+                    weights = np.maximum(weights, [0.05])
+                    weights = weights / np.sum(weights)
+                if weights.sum() != 1.0:
+                    weights = weights / np.sum(weights)
+
+                reweighted_idx = np.random.choice(x.size(0), x.size(0), p=weights, replace=True)
+                x_resampled = x[reweighted_idx]
+                top_idx = [ct for _, ct in Counter(reweighted_idx).most_common(5)]
+                #if np.random.rand() > 0.95:
+                #    print(top_idx)
+
+                # 3. Compute g for resampled observations
+                z_g, _, _, ldj_g, _ = model(x=x_resampled, components="c")
+                g_nll = -1.0 * (log_normal_standard(z_g, reduce=True, dim=-1, device=args.device) + ldj_g)
+                nll = torch.mean(g_nll)
+
+                if torch.isnan(z_g).any() or torch.isnan(g_nll).any():
+                    print("FAILURE")
+                    quit()
+            else:
+                z_g, _, _, ldj_g, _ = model(x=x, components="c")
+                g_nll = -1.0 * (log_normal_standard(z_g, reduce=True, dim=-1, device=args.device) + ldj_g)
+                nll = torch.mean(g_nll - G_nll)
+
+            losses = {"nll": nll}
             losses["G_nll"] = torch.mean(G_nll)
             losses["g_nll"] = torch.mean(g_nll)
+            
         else:
+            z_g, _, _, ldj_g, _ = model(x=x, components="c")
+            g_nll = -1.0 * (log_normal_standard(z_g, reduce=True, dim=-1, device=args.device) + ldj_g)
             losses = {"nll": torch.mean(g_nll)}
             losses["g_nll"] = torch.mean(g_nll)
             losses["G_nll"] = torch.zeros_like(losses['g_nll'])

models/boosted_flow.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import logging
 import torch.distributions as D
+from scipy.special import logsumexp
 
 from models.generative_flow import GenerativeFlow
 from models.glow import Glow
@@ -100,11 +101,9 @@ class BoostedFlow(GenerativeFlow):
         Estimate gradient with Monte Carlo by drawing sample zK ~ g^c and sample zK ~ G^(c-1), and
         computing their densities under the full model G^c
         """
-        #x = x.detach().to(self.args.device)
         z_g, mu_g, var_g, ldj_g, _ = self.forward(x=x, components="c")
-        g_nll = -1.0 * (log_normal_standard(z_g, reduce=False, device=self.args.device).view(z_g.shape[0], -1).sum(1, keepdim=False) + ldj_g)
-
-        return g_nll.data.detach()
+        g_ll = log_normal_standard(z_g, reduce=True, dim=-1, device=self.args.device) + ldj_g
+        return g_ll.data.detach()
 
     @torch.no_grad()
     def _rho_gradient_G(self, x):
@@ -112,16 +111,36 @@ class BoostedFlow(GenerativeFlow):
         Estimate gradient with Monte Carlo by drawing sample zK ~ g^c and sample zK ~ G^(c-1), and
         computing their densities under the full model G^c
         """
-        #x = x.detach().to(self.args.device)
         fixed = "-c" if self.all_trained else "1:c-1"
         z_G, mu_G, var_G, ldj_G, _ = self.forward(x=x, components=fixed)
-        G_nll = -1.0 * (log_normal_standard(z_G, reduce=False, device=self.args.device).view(z_G.shape[0], -1).sum(1, keepdim=False) + ldj_G)
-        
-        return G_nll.data.detach()
+        G_ll = log_normal_standard(z_G, reduce=True, dim=-1, device=self.args.device) + ldj_G
+        return G_ll.data.detach()
+
+    @torch.no_grad()
+    def _rho_gradients(self, x):
+        full_ll = torch.zeros(x.size(0))
+        fixed_ll = torch.zeros(x.size(0))
+        new_ll = torch.zeros(x.size(0))
+        for c in range(self.component + 1):
+            z, _, _, ldj, _ = self.forward(x=x, components=c)
+            if c == 0:
+                full_ll = log_normal_standard(z, reduce=True, dim=-1, device=self.args.device) + ldj
+                
+            else:
+                new_ll = log_normal_standard(z, reduce=True, dim=-1, device=self.args.device) + ldj
+                # compute full model using recursive formula
+                prev_ll = (torch.log(1 - self.rho[c]) + full_ll).view(x.size(0), 1)
+                next_ll = (torch.log(self.rho[c]) + new_ll).view(x.size(0), 1)
+                full_ll = torch.logsumexp(torch.cat([prev_ll, next_ll], dim=1), dim=1)
+
+            if c == self.component - 1:
+                fixed_ll = full_ll
+                
+        return new_ll, fixed_ll, full_ll
     
     def update_rho(self, data_loader):
         """
-        Learn weights rho using algorithm and updates from Section 3.3
+        Learn weights rho
         """
         if self.component == 0 and self.all_trained == False:
             return
@@ -140,7 +159,6 @@ class BoostedFlow(GenerativeFlow):
             init_step_size = self.args.rho_lr
             min_iters = 10
             max_iters = self.args.rho_iters
-            num_repeats = self.num_components
             prev_rho = self.rho[self.component].item()
 
             # create dataloader-iterator
@@ -157,15 +175,25 @@ class BoostedFlow(GenerativeFlow):
 
                 x = x.detach().to(self.args.device)
 
-                g_nll = torch.cat(num_repeats * [self._rho_gradient_g(x)])
-                G_nll = torch.cat([self._rho_gradient_G(x) for r in range(num_repeats)])
+                approximate=True
+                if approximate:
+                    #g_ll = self._rho_gradient_g(x)
+                    #G_ll = self._rho_gradient_G(x)
+                    # gradient descent moving in direction to minimize negative llhood of g relative to G
+                    g_ll, G_ll, _ = self._rho_gradients(x)
+                    gradient = torch.mean((-g_ll) - (-G_ll))
+                    loss_msg = f"\tg_ll - G_ll: ({g_nll.mean():6.1f} +/- {g_nll.std():3.1f}, {G_nll.mean():6.1f}  +/- {g_nll.std():3.1f})."
+                else:
+                    new_ll, fixed_ll, full_ll = self._rho_gradients(x)
+                    gradient = torch.mean((torch.exp(new_ll) - torch.exp(fixed_ll)) / torch.exp(full_ll)).data.detach()
+                    #numerator = logsumexp(np.array([fixed_ll.numpy(), new_ll.numpy()]), b=np.array([[1.0], [-1.0]]), axis=0)
+                    #gradient = -1.0 * torch.mean(torch.exp(numerator - full_ll)).data.detach()
+                    loss_msg = f"\tlog new={new_ll.mean():6.1f}, log fixed={fixed_ll.mean():6.1f}, log full={full_ll.mean():6.1f}"
                 
-                gradient = torch.mean(g_nll - G_nll)  # flipped sign here because g and G have flipped signs
                 step_size = init_step_size / (0.05 * batch_id + 1)
                 rho = min(max(prev_rho - step_size * gradient, 0.01), 100.0)
 
                 grad_msg = f'{batch_id: >3}. rho = {prev_rho:6.4f} -  {gradient:6.3f} * {step_size:7.5f} = {rho:6.4f} '
-                loss_msg = f"\tg_nll - G_nll: ({g_nll.mean():6.1f} +/- {g_nll.std():3.1f}, {G_nll.mean():6.1f}  +/- {g_nll.std():3.1f})."
                 print(grad_msg + loss_msg, file=rho_log)
 
                 self.rho[self.component] = rho

utils/utilities.py
@@ -9,19 +9,34 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+def softmax(x):
+    e_x = torch.exp(x - torch.max(x))
+    return e_x / torch.sum(e_x)
+
+
 def safe_log(z):
 	return torch.log(z + 1e-7)
 
 
 def init_log(args):
     log_format = '%(asctime)s : %(message)s'
-    if args.save_log:
-        filename = os.path.join(args.snap_dir, "log.txt")
-        print(f"Saving log output to file: {filename}")
-        logging.basicConfig(filename=filename, format=log_format, datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
-    else:
-        logging.basicConfig(format=log_format, level=logging.INFO)
+    filename = os.path.join(args.snap_dir, "log.txt")
+    print(f"Saving log output to file: {filename}")
 
+    if args.print_log:
+        #logging.basicConfig(filename=filename, format=log_format, datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
+        handlers = [
+            logging.FileHandler(filename),
+            logging.StreamHandler()
+        ]
+    else:
+        #logging.basicConfig(format=log_format, level=logging.INFO)
+        handlers = [logging.FileHandler(filename)]
+        
+    logging.basicConfig(level=logging.INFO,
+                        format=log_format,
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        handlers=handlers)
 
 
 def load(model, optimizer, path, args, init_with_args=False, scheduler=None, verbose=True):